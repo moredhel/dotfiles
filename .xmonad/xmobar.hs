436261090760a6f010164a7d8a0f3a2b5e226a1d
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-17 11:53:13 +0100

    minor changes for mini

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -5,7 +5,7 @@
 -- This is setup for dual 1920x1080 monitors, with the right monitor as primary
 -- This setup has been chonged to work an a laptop
 Config {
-    font = "xft:Fixed-6",
+    font = "xft:Fixed-5",
     bgColor = "#000110",
     fgColor = "#fffaaa",
     -- position = Static { xpos = 0, ypos = 0, width = 1152, height = 16 },
@@ -15,15 +15,15 @@ Config {
         -- Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         -- Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Network "enp0s25" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "wlp2s0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Date "%a %b %_d %l:%M" "date" 10,
 	Run Kbd [("us(dvorak)", "DV"), ("us", "US")],
-	Run BatteryP ["BAT1"] ["-t", "<left>%","-H","99","--high","green","-L","99","-l","red"] 10,
+	Run BatteryP ["BAT1"] ["-t", "<left>%","-H","65","--high","green","-L","25","-l","red"] 10,
 	Run Com "mpc" ["-f", "\"%artist% - %title% - %time%\""] "mpd" 1000,
         Run StdinReader
 
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "<fc=#00DD00>%kbd%</fc> - %StdinReader%}{<fc=#aFFFCC>| %date%</fc> | %EGPH%  %enp0s25%  %battery%"
+    template = "<fc=#00DD00>%kbd%</fc> - %StdinReader%}{<fc=#aFFFCC>| %date%</fc> | %EGPH%  %wlp2s0%  %battery%"
 }
