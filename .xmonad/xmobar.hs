1cb0d82acd6de9d3c9d977c813d96cdc5a09aaa2
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-19 18:49:43 +0100

    change config for ubuntu

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -5,7 +5,7 @@
 -- This is setup for dual 1920x1080 monitors, with the right monitor as primary
 -- This setup has been chonged to work an a laptop
 Config {
-    font = "xft:Fixed-5",
+    font = "xft:Fixed-7",
     bgColor = "#000110",
     fgColor = "#fffaaa",
     -- position = Static { xpos = 0, ypos = 0, width = 1152, height = 16 },
@@ -15,7 +15,7 @@ Config {
         -- Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         -- Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Network "wlp2s0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "wlan0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Date "%a %b %_d %l:%M" "date" 10,
 	Run Kbd [("us(dvorak)", "DV"), ("us", "US")],
 	Run BatteryP ["BAT1"] ["-t", "<left>%","-H","65","--high","green","-L","25","-l","red"] 10,
@@ -25,5 +25,5 @@ Config {
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "<fc=#00DD00>%kbd%</fc> - %StdinReader%}{<fc=#aFFFCC>| %date%</fc> | %EGPH%  %wlp2s0%  %battery%"
+    template = "<fc=#00DD00>%kbd%</fc> - %StdinReader%}{<fc=#aFFFCC>| %date%</fc> | %EGPH%  %wlan0%  %battery%"
 }
