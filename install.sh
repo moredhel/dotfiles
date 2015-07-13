81d5f0173bf157bddf48f4182b47f8f6a13bd49b
Hamish Hutchings <hamish@freeagent.com>
2015-07-13 11:47:36 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DOT_DIR="$HOME/.home"
+
+cd $HOME
+
+# Clone Dotfiles.
+git clone https://github.com/hamhut1066/dotfiles.git $DOT_DIR
+
+# Pull Dependencies.
+
+# Spacemacs
+git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# private layers
+git clone https://github.com/hamhut1066/private-layers.git $HOME/.spacemacs.private
+
+# Linking in config files
+ln -s $DOT_DIR/.zshrc $HOME/.zshrc
+ln -s $DOT_DIR/.spacemacs $HOME/.spacemacs
+
+touch $HOME/.zshrc_local
+# Notes
+
+echo "Link from $HOME/.spacemacs.private into $HOME/.emacs.d/private the layers that you want."
+echo "put config local to this computer in .zshrc_local"
